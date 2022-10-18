4cfd92621a2601c3cd341a744f1044cd5d31f601
O. Trukhin <mr.truhinoleg@yandex.ru>
2022-10-18 09:39:33 -0700

    added server port in settings.py

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -15,7 +15,8 @@ ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = [
     'http://*localhost',
     'http://*127.0.0.1',
-    'http://84.252.137.183'
+    'http://84.252.137.183',
+    'http://84.252.137.183:8080'
 ]
 
 
